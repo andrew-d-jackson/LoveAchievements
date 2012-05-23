7950d50cba08dccbdbc268f883e3b198ce342320
Andrew <andydjackson@gmail.com>
2012-05-23 21:12:05 +0100

    Added License

--- examples/KeyPresser/achievements/config.lua
+++ examples/KeyPresser/achievements/config.lua
@@ -4,7 +4,7 @@ function AchievementSystemConfig(achievementSystem)
 	achievementSystem:CreateAchievement("kp2", "P Presser", "Press P", "example2.jpg")
 	achievementSystem:CreateAchievement("kp3", "WASD Beginner", "Press W", "example2.jpg")
 
-	achievementSystem:DisableIntro()
+	--achievementSystem:DisableIntro()
 	--achievementSystem:SetBackgroundColor(0, 0, 0)
 	--achievementSystem:SetUnlockedColor(255, 255, 0)
 	--achievementSystem:SetLockedColor(0, 0, 0)
